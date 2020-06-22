9177244d88e123a4d697097be0443244d5ca85cf
ChunKoo Park <mix1009@gmail.com>
2020-06-22 15:08:40 +0900

    basic window functions

--- init.lua
+++ init.lua
@@ -55,3 +55,35 @@ h_bind("t", launchApp("iTerm"))
 h_bind("z", launchApp("Finder"))
 h_bind("n", launchApp("Notes"))
 
+
+
+-- window functions
+
+function positionWindow(x, y, w, h)
+    return function()
+        local win = hs.window.focusedWindow()
+        if win == nil then return end
+        local f = win:frame()
+        local s = win:screen():frame()
+        f.x = s.x + s.w * x
+        f.y = s.y + s.h * y
+        f.w = s.w * w
+        f.h = s.h * h
+        -- hs.alert.show(s)
+        win:setFrame(f)
+    end
+end
+
+hs.window.animationDuration = 0
+
+h_bind("1", positionWindow(0, 0, 1/2, 1))
+h_bind("2", positionWindow(1/2, 0, 1/2, 1))
+
+h_bind("3", positionWindow(0, 0, 1/2, 1/2))
+h_bind("4", positionWindow(1/2, 0, 1/2, 1/2))
+h_bind("5", positionWindow(0, 1/2, 1/2, 1/2))
+h_bind("6", positionWindow(1/2, 1/2, 1/2, 1/2))
+
+h_bind("q", positionWindow(0, 0, 2/3, 1))
+h_bind("w", positionWindow(2/3, 0, 1/3, 1))
+
